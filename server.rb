20e01051d69bf18f18b8b2d7d2517312c3299802
Giancarlo <guerragiancarlo8@gmail.com>
2015-11-03 15:24:20 +0100

    implemented divide

--- server.rb
+++ server.rb
@@ -13,7 +13,13 @@ get "/multiply" do
   erb(:multiply)
 end
 
+get "/divide" do 
+  erb(:divide)
+end
 
+post "/calculate_divide" do 
+  Calculator.new.divide_numbers(params[:first_number].to_i, params[:second_number].to_i)
+end
 
 post "/calculate_add" do
   Calculator.new.add_numbers(params[:first_number].to_i, params[:second_number].to_i)  
