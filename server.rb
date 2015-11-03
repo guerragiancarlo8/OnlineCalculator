b52e50a266c390e83dcbc72c5217d9eee5c842ab
Giancarlo <guerragiancarlo8@gmail.com>
2015-11-03 14:36:54 +0100

    basic add functionality. first iteration complete

--- server.rb
+++ server.rb
@@ -3,3 +3,11 @@ require "sinatra"
 get "/add" do
   erb(:add)
 end
+
+post "/calculate_add" do
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first + second
+  "#{first} + #{second} = #{result}"
+
+end
