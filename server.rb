5c96c49ee48a5cd66307cb325927d3553d96db90
Giancarlo <guerragiancarlo8@gmail.com>
2015-11-03 15:15:58 +0100

    subtract complete

--- server.rb
+++ server.rb
@@ -5,6 +5,14 @@ get "/add" do
   erb(:add)
 end
 
+get "/subtract" do 
+  erb(:subtract)
+end
+
 post "/calculate_add" do
   Calculator.new.add_numbers(params[:first_number].to_i, params[:second_number].to_i)  
+end
+
+post "/calculate_subtract" do 
+  Calculator.new.subtract_numbers(params[:first_number].to_i, params[:second_number].to_i)
 end
