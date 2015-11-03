b9e132ffd6c4d7a03309798156813d53829bc9d0
Rafael Ramon Fraga <rafael_ramon_fraga@yahoo.es>
2015-11-03 16:34:29 +0100

    Diferentes errors to check in the home page

--- server.rb
+++ server.rb
@@ -17,6 +17,33 @@ get "/divide" do
   erb(:divide)
 end
 
+get "/" do 
+  erb(:home)
+end
+
+post "/" do
+  operation = params["operation"]
+  case operation
+  when "addition"
+    get "/calculate_add" do
+      Calculator.new.divide_numbers(params[:first_number].to_i, params[:second_number].to_i)
+    end
+
+    post "/calculate_add" do
+      Calculator.new.divide_numbers(params[:first_number].to_i, params[:second_number].to_i)
+    end
+  when "subtraction"
+    redirect to("/calculate_subtract")
+  when "multiplication"
+    redirect to("/calculate_multiply")
+  when "division"
+    redirect to("/calculate_divide")
+  else
+    "Nothing works!"
+  end
+end
+
+
 post "/calculate_divide" do 
   Calculator.new.divide_numbers(params[:first_number].to_i, params[:second_number].to_i)
 end
